6ec8f34d242259742034d847f83231b2187392bd
maxime cluchague <mcluchag@taillat.inria.fr>
2017-06-21 15:25:07 +0200

    ajout du serveur glassfish / app java script

---
+++ hephaistos_project/src/com/za/websocket/SensorMessage.java
@@ -0,0 +1,22 @@
+package com.za.websocket;
+
+public class SensorMessage {
+	private String name;
+	private String message;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
